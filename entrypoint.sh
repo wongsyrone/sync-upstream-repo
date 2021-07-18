ad8465f68d9401b3aca3845c5d9dc2873a2dcf31
135e2 <51870710+135e2@users.noreply.github.com>
2021-07-18 18:42:57 +0800

    [WIP]Fix push

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,9 +36,9 @@ cd work || { echo "Missing work dir" && exist 2 ; }
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+#git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY_URL"
+#git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY_URL"
 git remote add upstream "$UPSTREAM_REPO"
 git fetch upstream #$UPSTREAM_BRANCH:$UPSTREAM_BRANCH $UPSTREAM_BRANCH
 git remote -v
